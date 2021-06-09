fd9054607f2661203f61c40ccf0486f6e7856b34
Gabriel Lamas <gabrielldarts@gmail.com>
2021-06-09 12:01:42 -0300

    Create style module for App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import Input from './components/Input'
 import useForm from './customHook/useForm'
 import GetCoords from './utils/getCoords'
-
+import Styles from './App.module.css';
 
 const App = () =>{
   const cityInput = useForm();
 
+  const getWeatherByCity = ()=>{
+    if(cityInput.validate()){
+      console.log('Vamos buscar')
+    }
+  }
+
+  const handleGetCoords = ()=>{
+    GetCoords()
+  }
+
   return (
-    <div className="App">
-      <h1>Previsão do tempo</h1>
+    <div className={Styles.Wrapper}>
+      <h1 className={Styles.Title}>Previsão do tempo</h1>
 
-        <Input label="Digite um nome" type="text" {...cityInput}/>
-        <button type="submit" onClick={()=>cityInput.validate()}>Buscar</button>
+      <Input label="Digite uma cidade" type="text" placeholder="Ex: London, São Paulo" {...cityInput}/>
+      <button type="submit" onClick={getWeatherByCity}>Buscar</button>
 
-      <p>ou</p>
+      <p className={Styles.Divisor}>ou</p>
 
-      <label>Pela minha localização</label><br/>
-      <button type="button" onClick={()=>GetCoords()}>Liberar minha localização</button>
+      <label className={Styles.Label}>Pela minha localização</label>
+      <button type="button" onClick={handleGetCoords}>Liberar minha localização</button>
     </div>
   )};
 
--- src/customHook/useForm.js
+++ src/customHook/useForm.js
@@ -7,7 +7,7 @@ const useForm = (type) => {
   function validate(value) {
     if (type === false) return true;
     if (value.length === 0) {
-      setError('Preencha um valor.');
+      setError('Digite um nome de cidade');
       return false;
     } else {
       setError(null);
