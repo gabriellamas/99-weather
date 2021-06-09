7efb56e02ea361ccc6168c6728f4753612b3687b
Gabriel Lamas <gabrielldarts@gmail.com>
2021-06-09 11:27:55 -0300

    Create input component and custom hook useForm for validation

---
+++ src/customHook/useForm.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+const useForm = (type) => {
+  const [value, setValue] = useState('');
+  const [error, setError] = useState(null);
+
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError('Preencha um valor.');
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
+  function onChange({ target }) {
+    setValue(target.value);
+  }
+
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+  };
+};
+
+export default useForm;
