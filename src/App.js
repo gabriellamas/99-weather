b62c0e86056f64fe5333e6d438e766f0061e970e
Gabriel Lamas <gabrielldarts@gmail.com>
2021-06-08 21:15:29 -0300

    Create ugly code for test the lat long requisiton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+const getLocation = () =>{
+  navigator.geolocation.getCurrentPosition(
+    async (position) => {
+      const lat = position.coords.latitude;
+      const long = position.coords.longitude;
+      window.localStorage.setItem(
+        'coords',
+        JSON.stringify({ lat, long }),
+      );
+      console.log({ lat, long });
+    },
+    (responseError) => {
+      // eslint-disable-next-line
+      console.log(responseError);
+    },
+  );
+}
+
 
 const App = () =>(
     <div className="App">
-      oi 
+      <h1>Previsão do tempo</h1>
+      <input type="text" placeholder="Insira o nome da cidade"/>
+      <p>ou</p>
+
+      <label>Pegar pela minha localização</label><br/>
+      <button type="button" onClick={()=>getLocation()}>Liberar minha localização</button>
     </div>
   );
 
